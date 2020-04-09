9a0c2e81bc64e741681c558e650618e94887cf41
Keletso A. Botsalano <kbotsalano@burs.org.bw>
2020-04-09 20:43:15 +0200

    initial commit

---
+++ Repositories/RepositoryBase.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WiseInventionsDataLayer.ServiceContracts;
+
+namespace WiseInventionsDataLayer.Repositories
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly DbContext _dbContext;
+        public RepositoryBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<T> FindAll() => _dbContext.Set<T>().AsNoTracking();
+
+        public IQueryable<T> FindPaginatedByConditionOrdered(int pageNumber, int entitiesPerPage, out int totalCount, Expression<Func<T, object>> include,
+            Func<T, object> orderExpression = null, Expression<Func<T, bool>> queryExpression = null)
+        {
+            var queryable = _dbContext.Set<T>().AsQueryable().Include("CreatedBy");
+
+            if (include != null)
+                queryable = queryable.Include(include);
+
+            if (queryExpression != null)
+                queryable = queryable.Where(queryExpression);
+
+            if (orderExpression != null)
+                queryable = queryable.OrderByDescending(orderExpression).AsQueryable();
+
+            totalCount = queryable.Count();
+
+            return queryable.Skip(entitiesPerPage * (Convert.ToInt32(pageNumber) - 1)).Take(entitiesPerPage);
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression) => _dbContext.Set<T>().Where(expression).AsNoTracking();
+       
+        public async Task Create(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry<T>(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Entry<T>(entity).State = EntityState.Deleted;
+        }
+    }
+}
---
+++ ServiceContracts/IRepositoryBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiseInventionsDataLayer.ServiceContracts
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindPaginatedByConditionOrdered(int pageNumber, int entitiesPerPage, out int totalCount, Expression<Func<T, object>> include, Func<T, object> orderExpression = null, Expression<Func<T, bool>> queryExpression = null);
+        IQueryable<T> FindAll();
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        Task Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
